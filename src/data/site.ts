cf6ad0ce8c2e105aa3902243f89a22b3bdd8d7f4
Empyrz901 <105421908+Empyrz901@users.noreply.github.com>
2026-08-28 18:27:59 +0200

    Add local business schema and improve address details in site layout

--- src/data/site.ts
+++ src/data/site.ts
@@ -9,10 +9,15 @@ export const business = {
     { display: "+41 77 901 09 32", whatsappNumber: "41779010932" },
     { display: "+41 76 536 21 09", whatsappNumber: "41765362109" }
   ],
+  streetAddress: "Avenue de Grandson 43",
+  postalCode: "1400",
+  addressLocality: "Yverdon-les-Bains",
+  addressCountry: "CH",
   address: "Avenue de Grandson 43 - Yverdon-les-Bains",
   partner: "Partenaire Auto Center",
   locationImage: "/images/auto-center-yverdon.webp",
   instagram: "@detailing_factory_ch",
+  instagramUrl: "https://www.instagram.com/detailing_factory_ch/",
   googleRating: "5.0",
   mapUrl: "https://www.google.com/maps/search/?api=1&query=Avenue%20de%20Grandson%2043%2C%20Yverdon-les-Bains"
 };
